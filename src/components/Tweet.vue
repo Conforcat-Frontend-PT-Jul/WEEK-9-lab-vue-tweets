13e08dae5a07baf516384e5c0bd00170df10c30f
Vicente <vmedina@omitsis.com>
2022-09-24 15:24:48 +0200

    :sparkles: added actions component and endt iteration 3

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -8,13 +8,7 @@
       </div>
       <Message :message="tweet.message" />
 
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <Actions />
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
@@ -26,6 +20,7 @@ import ProfileImage from "./ProfileImage.vue";
 import User from "./User.vue";
 import Timestamp from "./Timestamp.vue";
 import Message from "./Message.vue";
+import Actions from "./Actions.vue";
 export default {
   props: {
     tweet: Object,
@@ -35,6 +30,7 @@ export default {
     User,
     Timestamp,
     Message,
+    Actions,
   },
 };
 </script>
