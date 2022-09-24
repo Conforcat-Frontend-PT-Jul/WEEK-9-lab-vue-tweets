ba115d07cdb5917dee67a44bfffe997cada634b4
alelof <alejandra.lof@gmail.com>
2022-09-24 17:36:17 +0200

    [BCN-PT-072022] AlejandraLopez

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+  image: {
+    type: String,
+  },
+});
+</script>
+<template>
+  <img class="profile" :src="image" alt="profile" />
+</template>
---
+++ src/components/User.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+defineProps({
+    userData: {
+        type: Object,
+    },
+});
+
+</script>
+<template>
+    <span className="user">
+        <span className="name">{{ userData.name }}</span>
+        <span className="handle">{{ userData.handler }}</span>
+    </span>
+</template>
