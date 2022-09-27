10c80187ece7be71b3c408ea641f702041323542
ekatemi <ekatemi@gmail.com>
2022-09-27 02:13:24 +0200

    finished

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,28 +1,15 @@
 <template>
   <div className="tweet">
-    <img :src="tweet[0].user.image" className="profile" alt="profile" />
+    <ProfileImage :image="user.image" />
 
     <div className="body">
       <div className="top">
-        <span className="user">
-          <span className="name"> {{ tweet[0].user.name }} </span>
-          <span className="handle"> {{ tweet[0].user.handle }} </span>
-        </span>
-
-        <span className="timestamp"> {{ tweet[0].timestamp }} </span>
+        <User :userData="user" />
+        <Timestamp :time="timestamp" />
       </div>
 
-      <p className="message">
-        {{ tweet[0].message }}
-      </p>
-
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <Message :message="message" />
+      <Actions />
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
@@ -34,8 +21,17 @@ a {
   color: #42b983;
 }
 </style>
+
 <script setup>
+import ProfileImage from "./ProfileImage.vue";
+import User from "./User.vue";
+import Timestamp from "./Timestamp.vue";
+import Message from "./Message.vue";
+import Actions from "./Actions.vue";
+
 const props = defineProps({
-  tweet: { type: Object },
+  user: { type: Object },
+  timestamp: { type: String },
+  message: { type: String },
 });
 </script>
