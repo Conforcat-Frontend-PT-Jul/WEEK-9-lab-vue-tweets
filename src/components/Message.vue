a9afd1763e756a6838aa1eb97cceb0ef26e92c64
Mihhedkol <mihhdekol@gmail.com>
2022-09-27 16:05:03 +0200

    done

---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+    props: {
+    message: String,
+    },
+};
+
+</script>
+  
+  <template>
+   <p className="message"> {{ message }} </p>
+  </template>
+  
+  <style scoped>
+    
+  </style>
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+    props: {
+    image: String,
+    },
+};
+
+</script>
+  
+  <template>
+    <img
+      :src="image"
+      className="profile"
+      alt="profile"
+    />
+  </template>
+  
+  <style scoped>
+    
+  </style>
+  
