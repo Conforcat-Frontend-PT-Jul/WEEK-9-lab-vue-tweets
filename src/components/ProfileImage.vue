80408dfbc82443369fe44079884266bec3b93b94
Lua Varela <lua_varela12@hotmail.com>
2022-09-30 01:18:07 +0200

    Me atasqué en la Iteration 2 pero después de aclarar pude seguir

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    props: {
+        image: String,
+    },
+};
+</script>
+
+<template>
+    <img :src="image" className="profile" alt="profile" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    props: {
+        userData: Object,
+    },
+};
+</script>
+
+<template>
+    <span className="user">
+        <span className="name">{{ userData.name }}</span>
+        <span className="handle">@{{ userData.handle }}</span>
+    </span>
+</template>
+
+<style scoped>
+
+</style>
