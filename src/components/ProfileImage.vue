0678bfbf1797daa4ef4af8b52da36462cfe02791
danalmorse <110061505+danalmorse@users.noreply.github.com>
2022-10-01 00:54:08 +0200

    Lab done with iterations

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const props = defineProps({
+      image: {
+        type: String,
+      },
+    });
+</script>
+
+<template>
+      <img
+      v-bind:src="image" 
+      class="profile" 
+      alt="profile" />
+</template>
+
+<style scoped>
+    
+</style>
--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,33 +1,54 @@
+<script setup>
+  import ProfileImage from './ProfileImage.vue';
+  import User from './User.vue';
+  import Timestamp from './Timestamp.vue';
+  import Message from './Message.vue';
+  import Actions from './Actions.vue';
+
+
+  const props = defineProps({
+    user: { type: Object, },
+    timestamp: { type: String, },
+    message: { type: String, },
+});
+  /*Puedo utilizar el props solo de tweet= Object? pero claro en cada parte debajo en el HTML debo colocar tweet.user.image? y tweet.user, tweet.timestamp?  */
+</script>
+
 <template>
   <div className="tweet">
-    <img
+    <ProfileImage v-bind:image="user.image"/>
+    <!--<img
       src="https://i.imgur.com/9yw1Fyw.jpg"
       className="profile"
       alt="profile"
-    />
+    />-->
 
     <div className="body">
       <div className="top">
-        <span className="user">
+        <User v-bind:userData="user" />
+        <!--<span className="user">
           <span className="name">Ironhack</span>
           <span className="handle">@ironhack</span>
-        </span>
+        </span>-->
 
-        <span className="timestamp">Nov 30, 2020</span>
+        <Timestamp v-bind:time="timestamp"/>
+       <!-- <span className="timestamp">Nov 30, 2020</span>-->
       </div>
 
-      <p className="message">
+      <Message v-bind:message="message"/>
+      <!--<p className="message">
         On December 7th, we will be hosting a #webinar that will introduce you
         to #SQL! Are you ready? 🚀
-      </p>
+      </p>-->
 
-      <div className="actions">
-        <!-- Font Awesome icons -->
+      <Actions />
+      <!-- <div className="actions">
+         Font Awesome icons 
         <i class="far fa-comment"></i>
         <i class="fas fa-retweet"></i>
         <i class="far fa-heart"></i>
         <i class="fas fa-share"></i>
-      </div>
+      </div>-->
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
