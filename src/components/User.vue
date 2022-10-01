70d92edb0f43a60c909a2a11534ac9729293ccc0
Leandro Balaguer <leandro.balaguer@gmail.com>
2022-10-01 15:18:49 +0200

    done!

---
+++ src/components/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+<span className="user" >
+<span className="name">{{ userData.name }}</span>
+ <span className="handle">{{ userData.handle }}</span>
+        </span>
+        </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      userData:Object,
+    },
+  };
+  </script>
