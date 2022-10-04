452a3cf3ea79aa2336cb85ee33187c034c79b6c0
jspsegarra <jspsegarra86@gmail.com>
2022-10-04 21:58:23 +0200

    🚀 lab done

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,33 +1,26 @@
-<template>
-  <div className="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      className="profile"
-      alt="profile"
-    />
+<script setup>
+  import ProfileImage from './ProfileImage.vue';
+  import User from './User.vue';
+  import Timestamp from './Timestamp.vue';
+  import Message from './Message.vue';
+  import Actions from './Actions.vue';
+  const props = defineProps({
+    user: { type: Object, },
+    timestamp: { type: String, },
+    message: { type: String, },
+  });
+  </script>
 
+  <template>
+  <div className="tweet">
+    <ProfileImage :image="user.image" />
     <div className="body">
       <div className="top">
-        <span className="user">
-          <span className="name">Ironhack</span>
-          <span className="handle">@ironhack</span>
-        </span>
-
-        <span className="timestamp">Nov 30, 2020</span>
+        <User :userData="user" />
+        <Timestamp :time="timestamp" />
       </div>
-
-      <p className="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
-
-      <div className="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
-      </div>
+      <Message :message="message" />
+      <Actions />
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
@@ -36,6 +29,6 @@
 
 <style scoped>
 a {
-  color: #42b983;
+  color: #1b754c;
 }
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,14 @@
+<script setup>
+    defineProps({
+        userData: {
+            type: Object,
+        },
+    });
+    </script>
+    
+    <template>
+        <span className="user">
+            <span className="name">{{ userData.name }}</span>
+            <span className="handle">{{ userData.handler }}</span>
+        </span>
+    </template>
